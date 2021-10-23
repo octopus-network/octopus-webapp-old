92035b796e617d50ba5ae382d5b715600342a9f8
Bill Tran <tvkhue96@gmail.com>
2021-10-24 00:53:06 +0700

    fix(tslint): remove redundant code - unused import

--- src/config/defaultTheme.js
+++ src/config/defaultTheme.js
@@ -1,4 +1,4 @@
-import { extendTheme, withDefaultColorScheme, withDefaultSize } from '@chakra-ui/react';
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
 const defaultTheme = {
   colors: {
