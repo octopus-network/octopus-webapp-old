5b7a4688e1f09aafcd8ffc3c0cf4df229dc175ca
czl1378 <czl1378@126.com>
2021-11-11 19:58:07 +0800

    Use .toFixed() instead of .toString() in the type conversion of BigNumber

--- src/utils/decimal.ts
+++ src/utils/decimal.ts
@@ -23,8 +23,8 @@ export class DecimalUtils {
     if (input.isNeg()) {
       throw new Error(`Negative decimal value ${input} cannot be converted to u64.`);
     }
-   
-    const shiftedValue = new BN(input.mul(new Decimal(10).pow(new Decimal(shift))).toString());
+
+    const shiftedValue = new BN(input.mul(new Decimal(10).pow(new Decimal(shift))).toFixed());
     return shiftedValue;
   }
 
