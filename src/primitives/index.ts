9c609bc6fb3a122637eed715cb19ecb949ba97cf
Yuanchao Sun <yuanchao.sun@gmail.com>
2022-01-30 21:48:21 +0800

    Update index.ts

--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -6,7 +6,7 @@ const T_GAS: BigNumber = new BigNumber(1).times(10 ** 12);
 
 export const Gas = {
   SIMPLE_CALL_GAS: T_GAS.times(50).toFixed(),
-  COMPLEX_CALL_GAS: T_GAS.times(150).toFixed()
+  COMPLEX_CALL_GAS: T_GAS.times(200).toFixed()
 }
 
 export const OCT_TOKEN_DECIMALS = 18;
